8fe5b9a65ac8b197de6e320a5540ec6cde087824
Kyle McDonald <hello@kylemcd.com>
2023-04-11 16:39:25 -0500

    📦 NEW: Tmux stuff

--- nvim/lua/kpm/init.lua
+++ nvim/lua/kpm/init.lua
@@ -2,32 +2,33 @@ require("kpm.set")
 require("kpm.remap")
 
 local augroup = vim.api.nvim_create_augroup
-local kpm  = augroup('kpm', {})
+local kpm = augroup("kpm", {})
 
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+local yank_group = augroup("HighlightYank", {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
-autocmd({"BufWritePre"}, {
-    group = kpm,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+autocmd({ "BufWritePre" }, {
+	group = kpm,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.o.term = "xterm-256color"
