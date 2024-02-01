704f4323601f76b485a00ec9ad802adb1b4a8b86
Kyle McDonald <kyle@knock.app>
2024-02-01 09:43:12 -0600

    feat: add noice command line

--- nvim/lua/kpm/lazy.lua
+++ nvim/lua/kpm/lazy.lua
@@ -1,23 +1,22 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
-
 local plugins = {
 	"wbthomason/packer.nvim",
 	"nvim-lua/plenary.nvim",
-     {
+	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.4",
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -49,6 +48,21 @@ local plugins = {
 		end,
 	},
 	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
+	{
 		"j-hui/fidget.nvim",
 		tag = "legacy",
 	},
@@ -57,21 +71,21 @@ local plugins = {
 		branch = "v1.x",
 		dependencies = {
 			-- LSP Support
-			 "neovim/nvim-lspconfig" ,
-			 "williamboman/mason.nvim" ,
-			 "williamboman/mason-lspconfig.nvim" ,
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 
 			-- Autocompletion
-			 "hrsh7th/nvim-cmp" ,
-			 "hrsh7th/cmp-buffer" ,
-			 "hrsh7th/cmp-path" ,
-			 "saadparwaiz1/cmp_luasnip" ,
-			 "hrsh7th/cmp-nvim-lsp",
-			 "hrsh7th/cmp-nvim-lua" ,
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
 
 			-- Snippgitsignsets
-			 "L3MON4D3/LuaSnip" ,
-			 "rafamadriz/friendly-snippets" ,
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
 
 			-- Make TS Errors not cut off
 			{
@@ -89,15 +103,15 @@ local plugins = {
 	"hrsh7th/cmp-nvim-lsp", -- for autocompletion
 	{
 		"nvimdev/lspsaga.nvim",
-        config = function()
-            require('lspsaga').setup({})
-        end,
+		config = function()
+			require("lspsaga").setup({})
+		end,
 		requires = {
-			 "nvim-tree/nvim-web-devicons" ,
-			 "nvim-treesitter/nvim-treesitter" ,
+			"nvim-tree/nvim-web-devicons",
+			"nvim-treesitter/nvim-treesitter",
 		},
 	},
-    -- enhanced lsp uis
+	-- enhanced lsp uis
 	"onsails/lspkind.nvim", -- VS code like pictograms
 	"jose-elias-alvarez/null-ls.nvim", -- configure formatters & linters
 	"jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
@@ -119,7 +133,7 @@ local plugins = {
 	"tpope/vim-surround", -- Wrap Things
 	-- Git
 	"tpope/vim-fugitive",
- --    {
+	--    {
 	-- 	"pwntester/octo.nvim",
 	-- 	requires = {
 	-- 		"nvim-lua/plenary.nvim",
@@ -152,7 +166,7 @@ local plugins = {
 	"rafamadriz/friendly-snippets", -- useful snippets
 	-- Search
 	"jremmen/vim-ripgrep",
-	"duane9/nvim-rg"
-};
+	"duane9/nvim-rg",
+}
 
 require("lazy").setup(plugins)
